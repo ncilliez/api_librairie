85b6b168f3321d41ea950322ef995535c331b627
Nicolas Cilliez <ncilliez@gmail.com>
2023-09-19 18:45:54 +0200

    function deleteLivre

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\LivreRepository; // N'oubliez pas d'importer LivreRepository
 use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Livre;
 
 class LivreController extends AbstractController
@@ -25,6 +26,15 @@ class LivreController extends AbstractController
     {
         $jsonLivre = $serializer->serialize($livre, 'json', ['groups' => 'getLivres']);
         return new JsonResponse($jsonLivre, Response::HTTP_OK, ['accept' => 'json'], true);
+    }
+
+    #[Route('/api/livres/{id}', name: 'deleteLivre', methods: ['DELETE'])]
+    public function deleteLivre(Livre $livre, EntityManagerInterface $em): JsonResponse 
+    {
+        $em->remove($livre);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
 
